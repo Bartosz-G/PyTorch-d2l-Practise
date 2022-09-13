820d9c22b9dce7339e71f0ae9dace2f4ab3d13e2
Bartosz Gawin <bartex.gw@gmail.com>
2022-09-13 14:04:27 +0200

    V1.04

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import torch
-import pandas as pd
-
-
-
-hptrain = pd.read_csv(Datasets/hptrain.csv)
-
+def load_array(data_arrays, batch_size, is_train=True):  #@save
+    """Construct a PyTorch data iterator."""
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
 #%%
-
-
-hptrain
-
+data_iter = load_array((olstrain.loc[:,olstrain.columns != "SalePrice"],olstrain.loc[:,"SalePrice"]),10)
+#%%
+for epoch in range(3):
+    for X, y in data_iter:
+        l = loss(net(X) ,y)
+        trainer.zero_grad()
+        l.backward()
+        trainer.step()
+    l = loss(net(features), labels)
+    print(f'epoch {epoch + 1}, loss {l:f}')
 #%%
